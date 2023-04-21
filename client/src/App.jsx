8e0ab46f573ea63a013d2904c9300c7c256b06c5
JavierPineyro <javidunam22@gmail.com>
2023-04-21 17:48:09 -0300

    Refactor init files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,22 @@
-import { useState, useEffect } from 'react'
-import './App.css'
 
 // http://localhost:3333/2cYx2nDbVV4mb0H-r7UUI
 
 function App() {
-  const [res, setRes] = useState(0)
-
-  useEffect(() => {
-    const data = { originalUrl: 'https://twitter.com' }
-    fetch('/api/short', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+  // useEffect(() => {
+  //   const data = { originalUrl: 'https://twitter.com' }
+  //   fetch('/api/short', {
+  //     method: 'POST',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
 
   return (
     <div className='App'>
-      {res.originalURL}
+      <h1>Shurly</h1>
     </div>
   )
 }
