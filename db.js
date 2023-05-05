8a016c5a3a6738c3e6877ff8d3f2023c9827df5e
JavierPineyro <javidunam22@gmail.com>
2023-05-05 18:43:48 -0300

    Change DB url

--- db.js
+++ db.js
@@ -2,9 +2,13 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const username = process.env.DB_USERNAME
+const pass = process.env.DB_PASS
+const MONGODB_URI = `mongodb+srv://${username}:${pass}@shurly-url.rnzosvh.mongodb.net/shurlydb?retryWrites=true&w=majority`
+
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
